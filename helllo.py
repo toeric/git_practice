45fbd941108891a2dc2e0e785bfd89aff26d1ece
eric <erictotu@gmail.com>
2024-11-24 11:38:22 +0800

    show love in main

--- helllo.py
+++ helllo.py
@@ -2,11 +2,19 @@ import sys
 
 def say_hello_world():
 	print("Hello World")
+	return
 
 def say_hello_to(name):
 	print("Hello " + name)
+	return
+
+def show_love(name1, name2):
+	print(name1 + " loves " + name2)
+	return
 
 say_hello_world()
 say_hello_to("Novem")
 say_hello_to("Jocelyn")
 say_hello_to("Penguin")
+
+show_love("Novem", "Jocelyn")
