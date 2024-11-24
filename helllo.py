0da563c1e4e7c41d0d5dc961a0bf3e9179c60b73
eric <erictotu@gmail.com>
2024-11-24 11:51:57 +0800

    add century egg

--- helllo.py
+++ helllo.py
@@ -16,6 +16,10 @@ def show_love(name1, name2):
 	print(name1 + " loves " + name2)
 	return
 
+def show_love_to_century_egg(name):
+	print(name + " loves Century Egg")
+	return
+
 say_hello_world()
 say_hello_to("Novem")
 say_hello_to("Jocelyn")
@@ -24,3 +28,6 @@ say_hello_to("Penguin")
 show_love("Novem", "Jocelyn")
 show_love("Novem", "Penguin")
 show_love("Penguin", "Jocelyn")
+
+show_love_to_century_egg("Novem")
+show_love_to_century_egg("Jocelyn")
